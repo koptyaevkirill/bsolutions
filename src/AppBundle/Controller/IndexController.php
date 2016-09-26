fe73a0318cabd972a218cf4ada3493d7b7f3bcfc
koptyaevkirill <koptyaevkiril@gmail.com>
2016-09-26 19:45:32 +0300

    Authorization

--- src/AppBundle/Controller/IndexController.php
+++ src/AppBundle/Controller/IndexController.php
@@ -5,12 +5,20 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class IndexController extends Controller {
+class IndexController extends Controller
+{
     /**
      * @Route("/", name="homepage")
      * @Template("AppBundle:Index:index.html.twig")
      */
-    public function indexAction(Request $request) {
-        return [];
+    public function indexAction(Request $request)
+    {
+        $authenticationUtils = $this->get('security.authentication_utils');
+        $error = $authenticationUtils->getLastAuthenticationError();
+        $lastUsername = $authenticationUtils->getLastUsername();
+        return $this->render('AppBundle:Index:index.html.twig', array(
+            'last_username' => $lastUsername,
+            'error'         => $error,
+        ));
     }
 }
